00c6b08b10893a9b14dff4815f7a54699a930f70
Angelo Lamacchia <angelo.lamacchia@live.it>
2021-05-01 18:39:49 +0200

    corretto es con chiamate php

--- Ver_AJAX/js/master.js
+++ Ver_AJAX/js/master.js
@@ -7,15 +7,9 @@ var app = new Vue ({
         selected: '',
     },
 
-    mounted() {
-        axios
-        //chiamata get dinamica
-        .get(window.location.protocol + '//' + window.location.hostname + ':' + window.location.port + '/php-ajax-dischi/Ver_PHP/partials/server.php')
-        .then( (result) => {
-
-            this.songs = result.data;
-        }); 
-        
+    created() {
+        this.getAlbums();
+        this.getAuthors();
     },
 
     methods: {
@@ -24,16 +18,32 @@ var app = new Vue ({
             this.songs.sort( (a,b)=>(parseInt(a.year) < parseInt(b.year) ) ? -1:1);
         },
 
+        getAlbums() {
+            axios
+            //chiamata get dinamica
+            .get(window.location.protocol + '//' + window.location.hostname + ':' + window.location.port + '/php-ajax-dischi/Ver_PHP/partials/server.php')
+            .then( (result) => {
+                this.songs = result.data;
+            }); 
+        },
+
         getAuthors() {
             //chiamata per gli autori
             axios
-            .get(window.location.protocol + '//' + window.location.hostname + ':' + window.location.port + '/php-ajax-dischi/Ver_PHP/partials/server.php?listAuthor=true')
+            .get(window.location.protocol + '//' + window.location.hostname + ':' + window.location.port + '/php-ajax-dischi/Ver_PHP/partials/server.php?getAuthor=all')
             .then((result) => {
-
                 this.songsArtist = result.data;
             });
+        },
+
+        getSingleAuthor() {
+            //chiamata per l'album di uno specifico autore
+            axios
+            .get(window.location.protocol + '//' + window.location.hostname + ':' + window.location.port + '/php-ajax-dischi/Ver_PHP/partials/server.php?getAuthor=' + this.selected)
+            .then((result) => {
+                this.songs = result.data;
+            });
         }
-
-
     }
+
 });
--- Ver_PHP/partials/server.php
+++ Ver_PHP/partials/server.php
@@ -1,34 +1,67 @@
 <?php
     require __DIR__. '/database.php';
 
+    //metodo prof
     //tramite questa chiamata get ottengo la lista degli autori
-    if (!empty($_GET['listAuthor'])) {
+    // if (!empty($_GET['listAuthor'])) {
 
-        $listAuthor = $_GET['listAuthor'];
+    //     $listAuthor = $_GET['listAuthor'];
 
-        $authors = [];
+    //     $authors = [];
 
-        //se listAuthor è vero allora popolo l'array authors
-        if ($listAuthor == true) {
+    //     //se listAuthor è vero allora popolo l'array authors
+    //     if ($listAuthor == true) {
 
-            foreach($database as $album) {
-                //scorro nel database, controllo che l'autore non ci sia
-                //e popolo l'array authors
-                if (!in_array($album['author'], $listAuthor)) {
-                    $authors[] = $album['author'];
-                }
+    //         foreach($database as $album) {
+    //             //scorro nel database, controllo che l'autore non ci sia
+    //             //e popolo l'array authors
+    //             if (!in_array($album['author'], $listAuthor)) {
+    //                 $authors[] = $album['author'];
+    //             }
 
+    //         }
+    //     }
+    //     //ottengo gli autori in formato json
+    //     echo json_encode($authors);
+
+    // } else {
+    //     //se non faccio la chiamata get ottengo tutto il database
+    //     header('Content-Type: application/json');
+
+    //     echo json_encode($database);
+    // }
+
+    //metodo mio
+    $getAuthor = $_GET['getAuthor'];
+    $albums = [];
+    
+    //se getAuthor è vuoto mi restituisce l'intero database
+    if (empty($getAuthor)) {
+        header('Content-Type: application/json');
+        echo json_encode($database);
+    } 
+    //se getAuthor è all mi restituisce gli autori
+    else if ($getAuthor == 'all') {
+        foreach ($database as $album) {
+            if (!in_array($album['author'], $authors)) {
+                $albums[] = $album['author'];
             }
         }
-        //ottengo gli autori in formato json
-        echo json_encode($authors);
-
-    } else {
-        //se non faccio la chiamata get ottengo tutto il database
-        header('Content-Type: application/json');
-
-        echo json_encode($database);
+        echo json_encode($albums);
+    } 
+    
+    //se getAuthor è il nome di un autore mi restituisce l'autore
+    foreach ($database as $album) {
+        if ($getAuthor == $album['author']) {
+            echo json_encode([$album]);
+        }
     }
+    
+
+    
+
+    
+    
 
     
 
